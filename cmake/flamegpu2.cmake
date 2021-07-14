f577de412b32ae004de591f1ed5598dfb798c9ee
Peter Heywood <peethwd@gmail.com>
2021-07-14 18:39:03 +0100

    CMake tidying + cpplint configuration file

--- cmake/flamegpu2.cmake
+++ cmake/flamegpu2.cmake
@@ -19,8 +19,17 @@ FetchContent_GetProperties(flamegpu2)
 if(NOT flamegpu2_POPULATED)
     FetchContent_Populate(flamegpu2)   
 
+    # Now disable extra bells/whistles and add flamegpu2 as a dependency
+    set(NO_EXAMPLES ON CACHE INTERNAL "-")
+    set(BUILD_TESTS OFF CACHE BOOL "-")
+    mark_as_advanced(FORCE BUILD_TESTS)
+
+    # Add the subdirectory
+    add_subdirectory(${flamegpu2_SOURCE_DIR} ${flamegpu2_BINARY_DIR})
+
     # Add flamegpu2' expected location to the prefix path.
     set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${flamegpu2_SOURCE_DIR}/cmake")
 endif()
-    message(STATUS ${flamegpu2_SOURCE_DIR})
-    set(FLAMEGPU_ROOT ${flamegpu2_SOURCE_DIR})
+
+message(STATUS "Found FLAMEGPU2 ${flamegpu2_SOURCE_DIR}")
+set(FLAMEGPU_ROOT ${flamegpu2_SOURCE_DIR})
