102efaf5fd4b310e6c48c7d8b34787cd6870267b
Peter Heywood <peethwd@gmail.com>
2021-08-09 10:56:04 +0100

    Update readme / CMake a little as prep for v2.0.0-alpha release

--- cmake/flamegpu2.cmake
+++ cmake/flamegpu2.cmake
@@ -1,14 +1,22 @@
 include(FetchContent)
 cmake_policy(SET CMP0079 NEW)
 
-# Allow the user to configure the version, until a stable release this should remain as master
-set(FLAMEGPU2_Version "master" CACHE STRING "Git branch or tag to use")
+# If a FLAMEGPU_VERSION has not been defined, set it to the default option.
+if(NOT DEFINED FLAMEGPU_VERSION OR FLAMEGPU_VERSION STREQUAL "")
+    set(FLAMEGPU_VERSION "master" CACHE STRING "Git branch or tag to use")
+endif()
+
+# Allow users to switch to forks with relative ease.
+
+if(NOT DEFINED FLAMEGPU_REPOSITORY OR FLAMEGPU_REPOSITORY STREQUAL "")
+    set(FLAMEGPU_REPOSITORY "https://github.com/FLAMEGPU/FLAMEGPU2.git" CACHE STRING "Remote Git Repository for FLAME GPU 2+")
+endif()
 
 # Always use most recent, simply recommend users that they may wish to do otherwise
 FetchContent_Declare(
     flamegpu2
-    GIT_REPOSITORY https://github.com/FLAMEGPU/FLAMEGPU2.git
-    GIT_TAG        ${FLAMEGPU2_Version}
+    GIT_REPOSITORY ${FLAMEGPU_REPOSITORY}
+    GIT_TAG        ${FLAMEGPU_VERSION}
     GIT_SHALLOW    1
     GIT_PROGRESS   ON
     # UPDATE_DISCONNECTED   ON
